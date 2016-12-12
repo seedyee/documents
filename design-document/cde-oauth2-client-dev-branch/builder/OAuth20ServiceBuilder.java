692cabddecbc62b729b9da55ca9e85aac5226486
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-12-12 15:12:30 +0800

    根据builder模式设计的builder Java类文件

---
+++ design-document/cde-oauth2-client-dev-branch/builder/OAuth20ServiceBuilder.java
@@ -0,0 +1,11 @@
+class RequestInfoBuilder {
+
+/**
+ * 获取 OAuth20Service 实例.
+ */
+getOAuth20Service() {
+    第一步：初始化 HttpClient（异步调用）
+    第二步：利用 ServiceBuilder.build() 获取到 OAuth20Service 的实例
+    第三步：将 OAuth20Service 实例返回出去
+  }
+}
