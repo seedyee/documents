df7a5297f23b1c926abbd162455dcf8aa97c8056
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-12-12 15:27:44 +0800

    修改伪代码中引入的包文件

--- design-document/cde-oauth2-client-dev-branch/client/OAuthClient.java
+++ design-document/cde-oauth2-client-dev-branch/client/OAuthClient.java
@@ -1,6 +1,3 @@
-import org.springframework.cloud.netflix.feign.FeignClient;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 //该类文件是调用cde-oauth-service的登录接口进行登录操作
 @FeignClient("authc-service")
 public interface OAuthClient {
