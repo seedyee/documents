3d80d2a61a333252e69992aa967cd1fc3c79403b
Fangcai Liao <liaofangcai@Fangcais-MacBook-Air.local>
2016-12-12 15:20:21 +0800

    设计操作redis缓存数据库的java类文件

---
+++ design-document/cde-oauth2-client-dev-branch/redis/RedisRepository.java
@@ -0,0 +1,18 @@
+
+  public class RedisRepository {
+
+  /**
+   * 从redis中获取值.
+   */
+  public String get(final String key) {}
+
+  /**
+   * 存储数据到redis中.
+   */
+  public void set(final String key, final String value) {}
+
+  /**
+   * 删除数据.
+   */
+  public void del(final String key) {}
+}
